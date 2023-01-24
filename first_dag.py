b599e99aa179a0be2c7b4df745eafedcd8354470
Haley Nelson <hmnelson13@gmail.com>
2023-01-24 04:16:06 +0000

    edit python file

--- first_dag.py
+++ first_dag.py
@@ -7,19 +7,19 @@ from airflow.operators.dummy import DummyOperator
 
 def first_task_function():
     log.info("Welcome to CS 280! This is your first task")
-    name = "Enter your name here"
+    name = "Haley Nelson"
     log.info(f"My name is {name}")
     return
 
 def second_task_function():
     log.info("This is your second task")
-    major = "Enter your name here"
+    major = "Computer Science"
     log.info(f"My major is {major}")
     return
 
 def third_task_function():
     log.info("This is your third task")
-    hometown = "Enter your hometown here"
+    hometown = "Aurora, Colorado"
     log.info(f"I am from {hometown}")
     return
 
@@ -30,14 +30,13 @@ with DAG(
     start_date=pendulum.datetime(2023, 1, 1, tz="US/Pacific"),
     catchup=False,
 ) as dag:
-
-
-
-start_task = DummyOperator(task_id="start_task")
-first_task = PythonOperator(task_id="first_task", python_callable=first_task_function)
-second_task = PythonOperator(task_id="second_task", python_callable=second_task_function)
-third_task = PythonOperator(task_id="third_task", python_callable=third_task_function)
-end_task = DummyOperator(task_id="end_task")
+    start_task = DummyOperator(task_id="start_task")
+    first_task = PythonOperator(task_id="first_task", python_callable=first_task_function)
+    second_task = PythonOperator(task_id="second_task", python_callable=second_task_function)
+    third_task = PythonOperator(task_id="third_task", python_callable=third_task_function)
+    end_task = DummyOperator(task_id="end_task")
 
 
 start_task >> first_task >> second_task >> third_task >> end_task
+
+
