6e7c1cf45c2dee4f7078935b243a95cfe1655782
Haley Nelson <hmnelson13@gmail.com>
2023-02-02 23:03:09 +0000

    debugging dag

--- get_twitter_api_data_task.py
+++ get_twitter_api_data_task.py
@@ -15,7 +15,8 @@ def get_users(ti: TaskInstance, **kwargs):
 	for user_id in users:
 		api_url = f"https://api.twitter.com/2/users/{user_id}?user.fields=public_metrics,profile_image_url,username,description,id"
 		request = requests.get(api_url, headers=get_auth_header())
-		ti.xcom_push("list_of_users", request.json())
+		user_requests.append(request)
+	ti.xcom_push("list_of_users", user_requests.json())
 	return
 
 def get_tweets(ti: TaskInstance, **kwargs):
