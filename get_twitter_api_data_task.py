a26ffff52938b59e7c758dcce886a1a1ffd60b6d
Haley Nelson <hmnelson13@gmail.com>
2023-02-02 04:59:34 +0000

    more edits

--- get_twitter_api_data_task.py
+++ get_twitter_api_data_task.py
@@ -26,24 +26,24 @@ def get_tweets(ti: TaskInstance, **kwargs):
 		api_url = f"https://api.twitter.com/2/users/{user_id}?tweet.fields=public_metrics,author_id,text"
 		request = requests.get(api_url, headers=get_auth_header())
 		tweet_requests.append(request.json())
-	    	ti.xcom_push("list_of_tweets", tweet_requests.json)
-    	return
+		ti.xcom_push("list_of_tweets", tweet_requests.json)
+	return
 
 def read_and_print_users(ti: TaskInstance, **kwargs):
 	users_list = ti.xcom_pull(key="list_of_users", task_ids="get_users")
 	logging.info(users_list)
-    	return
+	return
 
 def read_and_print_tweets(ti: TaskInstance, **kwargs):
-   	 tweets_list = ti.xcom_pull(key="list_of_tweets", task_ids="get_tweets")
-   	 logging.info(tweets_list)
-    	return
+	tweets_list = ti.xcom_pull(key="list_of_tweets", task_ids="get_tweets")
+	logging.info(tweets_list)
+	return
 
 with DAG(
-    	dag_id="twitter_api",
-    	schedule_interval="0 10 * * *",
-    	start_date=pendulum.datetime(2023, 1, 1, tz="US/Pacific"),
-    	catchup=False,
+	dag_id="twitter_api",
+	schedule_interval="0 10 * * *",
+	start_date=pendulum.datetime(2023, 1, 1, tz="US/Pacific"),
+	catchup=False,
 ) as dag:
 start_task = PythonOperator(
 	task_id="get_users",
