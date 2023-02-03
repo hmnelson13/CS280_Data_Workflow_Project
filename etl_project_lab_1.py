20309af02ab5a8ef4290e08c56bed34fe51db21d
Haley Nelson <hmnelson13@gmail.com>
2023-02-03 00:04:07 +0000

    transform dag

--- etl_project_lab_1.py
+++ etl_project_lab_1.py
@@ -10,35 +10,34 @@ def get_auth_header():
 	my_bearer_token = Variable.get("TWITTER_BEARER_TOKEN")
 	return {"Authorization": f"Bearer {my_bearer_token}"}
 
-def get_users(ti: TaskInstance, **kwargs):
+def get_twitter_api_data_task(ti: TaskInstance, **kwargs):
 	users = Variable.get(f"TWITTER_USER_IDS", [], deserialize_json=True)
 	user_requests = []
 	for user_id in users:
 		api_url = f"https://api.twitter.com/2/users/{user_id}?user.fields=public_metrics,profile_image_url,username,description,id"
 		request = requests.get(api_url, headers=get_auth_header())
+		log.info(request)
 		user_requests.append(request.json())
 	ti.xcom_push("list_of_users", user_requests)
-	return
 
-def get_tweets(ti: TaskInstance, **kwargs):
 	tweets = Variable.get(f"TWITTER_TWEET_IDS", [], deserialize_json=True)
 	tweet_requests = []
 	for tweet_id in tweets:
 		api_url = f"https://api.twitter.com/2/tweets/{tweet_id}?tweet.fields=public_metrics,author_id,text"
 		request = requests.get(api_url, headers=get_auth_header())
+		log.info(request)
 		tweet_requests.append(request.json())
 	ti.xcom_push("list_of_tweets", tweet_requests)
 	return
 
-def read_and_print_users(ti: TaskInstance, **kwargs):
-	users_list = ti.xcom_pull(key="list_of_users", task_ids="get_users")
-	log.info(users_list)
-	return
+def transform_twitter_api_data_func(ti: TaskInstance, **kwargs):
+        users_list = ti.xcom_pull(key="list_of_users", task_ids="get_users")
+        tweets_list = ti.xcom_pull(key="list_of_tweets", task_ids="get_tweets")
 
-def read_and_print_tweets(ti: TaskInstance, **kwargs):
-	tweets_list = ti.xcom_pull(key="list_of_tweets", task_ids="get_tweets")
-	log.info(tweets_list)
-	return
+        df_users = pd.read_json(users_list)
+        df_tweets = pd.read_json(tweets_list)
+        return
+
 
 with DAG(
 	dag_id="twitter_api",
@@ -46,22 +45,14 @@ with DAG(
 	start_date=pendulum.datetime(2023, 1, 1, tz="US/Pacific"),
 	catchup=False,
 ) as dag:
-	start_task = PythonOperator(
-		task_id="get_users",
-		python_callable=get_users,
+	extract = PythonOperator(
+		task_id="extract",
+		python_callable=get_twitter_api_data_task,
 		provide_context=True)
-	first_task = PythonOperator(
-		task_id="get_tweets",
-		python_callable=get_tweets,
-		provide_context=True)
-	second_task = PythonOperator(
-		task_id="second_task",
-		python_callable=read_and_print_users,
-		provide_context=True)
-	third_task = PythonOperator(
-		task_id="third_task",
-		python_callable=read_and_print_tweets,
+	transform = PythonOperator(
+		task_id="transform",
+		python_callable=transform_twitter_api_data_func,
 		provide_context=True)
 
 
-start_task >> first_task >> second_task >> third_task
+extract >> transform
